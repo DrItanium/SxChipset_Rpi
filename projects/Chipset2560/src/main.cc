0811e090b3870c23f02112e1d09ed5ab8139b007
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-10-26 10:59:47 -0700

    Do an intermediate stash on reads from psram to external cache ram
    I believe there is corruption happening during reads directly into
    external ram. This never happens if the cache is onboard so I believe it
    is some form of timing!

--- projects/Chipset2560/src/main.cc
+++ projects/Chipset2560/src/main.cc
@@ -989,16 +989,22 @@ PSRAMBackingStore::setAddress(Address address) noexcept {
 }
 size_t
 PSRAMBackingStore::read(Address addr, uint8_t* storage, size_t count) noexcept {
-    setAddress(addr);
-    _link.beginTransaction(SPISettings{5'000'000, MSBFIRST, SPI_MODE0});
-    digitalWrite<Pins::PSRAM_EN, LOW>();
-    _link.transfer(0x03);
-    _link.transfer(static_cast<uint8_t>(addr >> 16));
-    _link.transfer(static_cast<uint8_t>(addr >> 8));
-    _link.transfer(static_cast<uint8_t>(addr));
-    _link.transfer(storage, count);
-    digitalWrite<Pins::PSRAM_EN, HIGH>();
-    _link.endTransaction();
+    uint8_t localStorage[count] = { 0 };
+    {
+        setAddress(addr);
+        _link.beginTransaction(SPISettings{5'000'000, MSBFIRST, SPI_MODE0});
+        digitalWrite<Pins::PSRAM_EN, LOW>();
+        _link.transfer(0x03);
+        _link.transfer(static_cast<uint8_t>(addr >> 16));
+        _link.transfer(static_cast<uint8_t>(addr >> 8));
+        _link.transfer(static_cast<uint8_t>(addr));
+        _link.transfer(localStorage, count);
+        digitalWrite<Pins::PSRAM_EN, HIGH>();
+        _link.endTransaction();
+    }
+    {
+        (void)memcpy(storage, localStorage, count);
+    }
     return count;
 }
 
