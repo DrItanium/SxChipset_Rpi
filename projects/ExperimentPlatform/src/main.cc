a87cc94a6e9f0bd379f46310751337f77570662a
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-12-26 11:02:07 -0800

    Move back to a single source file

--- projects/ExperimentPlatform/src/main.cc
+++ projects/ExperimentPlatform/src/main.cc
@@ -93,6 +93,143 @@ const struct ush_descriptor ush_desc = {
     .hostname = hostname,
 };
 
+struct ush_node_object root;
+const struct ush_file_descriptor rootFiles[] = { };
+struct ush_node_object dev;
+const struct ush_file_descriptor devFiles[] = {
+    {
+        .name = "time", 
+        .description = nullptr,
+        .help = nullptr,
+        .exec = nullptr, 
+        .get_data = [](FILE_DESCRIPTOR_ARGS, uint8_t** data) noexcept { return sendDword(PASS_FILE_DESCRIPTOR_ARGS, data, millis()); },
+    },
+    {
+        .name = "micros",
+        .description = nullptr,
+        .help = nullptr,
+        .exec = nullptr, 
+        .get_data = [](FILE_DESCRIPTOR_ARGS, uint8_t** data) noexcept { return sendDword(PASS_FILE_DESCRIPTOR_ARGS, data, micros()); },
+    },
+#ifdef GPIOR0
+    ByteFile("gpr0", GPIOR0),
+#endif
+#ifdef GPIOR1
+    ByteFile("gpr1", GPIOR1),
+#endif
+#ifdef GPIOR2
+    ByteFile("gpr2", GPIOR2),
+#endif
+#ifdef GPIOR3
+    ByteFile("gpr3", GPIOR3),
+#endif
+#define AnalogFile(n, reg) { \
+        .name = n ,  \
+        .description = nullptr, \
+        .help = nullptr, \
+        .exec = nullptr, \
+        .get_data = [](FILE_DESCRIPTOR_ARGS, uint8_t** data) noexcept { return sendWord(PASS_FILE_DESCRIPTOR_ARGS, data, analogRead( reg )); }, \
+    } 
+#define X(pin) AnalogFile(#pin, pin)
+#include <AnalogPins.def>
+#undef X
+#undef AnalogFile
+};
+#define DefVPort(id) \
+const struct ush_file_descriptor gpioVPort ## id ## Files [] = { \
+    ByteFile("in", VPORT ## id . IN ), \
+    ByteFile("out", VPORT ## id . OUT ), \
+    ByteFile("dir", VPORT ## id . DIR ), \
+    ByteFile("intflags", VPORT ## id . INTFLAGS ), \
+}; \
+struct ush_node_object gpioVPort ## id ## Dir 
+#define X(id) DefVPort(id);
+#include <AVRPorts.def>
+#undef X
+#undef DefVPort
+
+struct ush_node_object specificCmd;
+const struct ush_file_descriptor specificCmdFiles[] = {
+    {
+        .name = "analogRead",
+        .description = nullptr,
+        .help = "usage: analogRead A[0-...]\r\n",
+        .exec = [](FILE_DESCRIPTOR_ARGS, int argc, char* argv[]) noexcept {
+            if (argc != 2) {
+                ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
+                return;
+            }
+            auto* arg1 = argv[1];
+#define X(letter) \
+            if (strcmp(arg1, #letter ) == 0) {  \
+                ush_printf(self, "%d\r\n", analogRead ( letter ) ); \
+                return; \
+            } 
+#include <AnalogPins.def>
+#undef X
+            // fallback condition
+            ush_printf(self, "Unknown analog device %s\r\n", arg1);
+        },
+    },
+    {
+        .name = "lsvport",
+        .description = nullptr,
+        .help = "usage: lsvport ?port-letter\r\n",
+        .exec = [](FILE_DESCRIPTOR_ARGS, int argc, char* argv[]) noexcept {
+            if (argc != 2) {
+                ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
+                return;
+            }
+            auto* arg1 = argv[1];
+#define X(letter) \
+            if (strcmp(arg1, #letter ) == 0) {  \
+                ush_printf(self, "\tIN: 0x%x\r\n", VPORT ## letter . IN); \
+                ush_printf(self, "\tOUT: 0x%x\r\n", VPORT ## letter . OUT); \
+                ush_printf(self, "\tDIR: 0x%x\r\n", VPORT ## letter . DIR); \
+                ush_printf(self, "\tINTFLAGS: 0x%x\r\n", VPORT ## letter . INTFLAGS); \
+                return; \
+            } 
+#include <AVRPorts.def>
+#undef X
+            ush_printf(self, "Unknown port %s\r\n", arg1);
+        },
+    },
+    {
+        .name = "lsport",
+        .description = nullptr,
+        .help = "usage: lsvport ?port-letter\r\n",
+        .exec = [](FILE_DESCRIPTOR_ARGS, int argc, char* argv[]) noexcept {
+            if (argc != 2) {
+                ush_print_status(self, USH_STATUS_ERROR_COMMAND_WRONG_ARGUMENTS);
+                return;
+            }
+            auto* arg1 = argv[1];
+#define Y(letter, idx) ush_printf(self, "\tPIN" #idx "CTRL: 0x%x\r\n", PORT ## letter . PIN ## idx ## CTRL )
+#define X(letter) \
+            if (strcmp(arg1, #letter ) == 0) {  \
+                ush_printf(self, "\tIN: 0x%x\r\n", PORT ## letter . IN); \
+                ush_printf(self, "\tOUT: 0x%x\r\n", PORT ## letter . OUT); \
+                ush_printf(self, "\tDIR: 0x%x\r\n", PORT ## letter . DIR); \
+                ush_printf(self, "\tINTFLAGS: 0x%x\r\n", PORT ## letter . INTFLAGS); \
+                ush_printf(self, "\tPORTCTRL: 0x%x\r\n", PORT ## letter . PORTCTRL); \
+                Y(letter, 0); \
+                Y(letter, 1); \
+                Y(letter, 2); \
+                Y(letter, 3); \
+                Y(letter, 4); \
+                Y(letter, 5); \
+                Y(letter, 6); \
+                Y(letter, 7); \
+                return; \
+            } 
+#include <AVRPorts.def>
+#undef X
+#undef Y
+            ush_printf(self, "Unknown port %s\r\n", arg1);
+        },
+    },
+};
+
 
 struct ush_node_object commonCmd;
 
@@ -139,18 +276,41 @@ computeRandomSeed() {
 extern void targetSpecificSetup();
 void 
 setup() {
-    targetSpecificSetup();
+    Serial1.swap(1);
+    Serial1.begin(115200);
     // setup a random source
     currentRandomSeed = computeRandomSeed();
 
     randomSeed(currentRandomSeed);
     ush_init(&ush, &ush_desc);
     ush_commands_add(&ush, &commonCmd, commonCmdFiles, NELEM(commonCmdFiles));
-    configureFileSystem(ush);
+    ush_commands_add(&ush, &specificCmd, specificCmdFiles, NELEM(specificCmdFiles));
+    ush_node_mount(&ush, "/", &root, rootFiles, NELEM(rootFiles));
+    ush_node_mount(&ush, "/dev", &dev, devFiles, NELEM(devFiles));
+#define RegisterPort(path, id) \
+    ush_node_mount(&ush, path , & gpioVPort ## id ## Dir , gpioVPort ## id ## Files , NELEM( gpioVPort ## id ## Files ))
+#define X(index) RegisterPort( "/dev/vport" #index , index );
+#include <AVRPorts.def>
+#undef X
+#undef RegisterPort
 }
 
 void 
 loop() {
     ush_service(&ush);
+}
+
+int 
+ushRead(struct ush_object* self, char* ch) {
+    if (Serial1.available() > 0) {
+        *ch = Serial1.read();
+        return 1;
+    }
+    return 0;
+}
+
+int 
+ushWrite(struct ush_object* self, char ch) {
+    return (Serial1.write(ch) == 1);
 }
 
