36a3ec602e7d4baa008794b00cdc40f5554196cf
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-02-02 12:57:35 -0800

    Enable serial connections

--- projects/Coprocessor2/src/main.cc
+++ projects/Coprocessor2/src/main.cc
@@ -1,7 +1,8 @@
 #include <Arduino.h>
-
+UART chipsetConnection(16, 17);
 void setup() {
-    Serial.begin(115200); // communication with the 2560 chipset
+    Serial.begin(9600);
+    chipsetConnection.begin(115200);
     pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, LOW);
 }
